fdc61fbc01a042a84a9c2859b72bd2483de410ce
Zachariah Ngonyani <zachariahn@gmail.com>
2017-10-15 22:12:49 +0300

    Add data push simulation to rabbitmq queue

--- app.js
+++ app.js
@@ -23,9 +23,16 @@ connection.on('ready', setup);
  * define an exchange
  */
 
+var bindingOptions =  { 'x-match' : 'all', 'Type'    : 'Feedback', 'Company' : '000' };
 var exchange;
+var queue;
 function setup() {
-  exchange = connection.exchange('my_exchange1', {'type': 'fanout', durable: false}, exchangeSetup);
+  exchange = connection.exchange('GLActualFeedbackExchange', {autoDelete: false, type: 'headers', passive: false, durable: true, arguments: bindingOptions}, function(exchange) {
+    console.log('creating/opening queue');
+    queue = connection.queue('GLActualFeedbackQueue', {autoDelete: false, durable: true, exclusive: false, arguments: bindingOptions}, function(queue) {
+      queue.bind_headers(exchange, bindingOptions);
+    });
+  });
 }
 
 /**
@@ -38,9 +45,7 @@ var queue;
 var deadQueue;
 
 function exchangeSetup() {
-  queue     =  connection.queue('my_queue1', {durable: false, exclusive: false}, queueSetup);
-  deadQueue =  connection.queue('my_queue2', {durable: false, exclusive: false}, queueSetup);
-
+  queue =  connection.queue('GLActualFeedbackQueue', {autoDelete: false, durable: true, exclusive: false, options: bindingOptions}, queueSetup);
   queue.on('queueBindOk', function() {
     onQueueReady(exchange);
   });
@@ -55,22 +60,67 @@ function queueSetup() {
     console.log("message from queue is", message);
     emitEvent(message.data);
   });
-  queue.bind(exchange.name, 'my_queue1');
-  deadQueue.bind(exchange.name, 'my_queue2');
+  queue.bind(exchange.name, '', {arguments: bindingOptions});
 }
 
 function onQueueReady(exchange) {
   console.log('queue binding done...');
 }
 
+var data = [
+  {
+    "id": 1138,
+    "Company": "2005",
+    "BookID": "MAINP",
+    "JournalCode": "DJ",
+    "CurrencyCode": "TZS",
+    "TrxCtrlNum": "Actualctr1138",
+    "ApplyDate": "2017-06-30T00:00:00",
+    "Description": "Description1138",
+    "DebitAcc": null,
+    "PositiveAmount": null,
+    "DrUID": null,
+    "CreditAcc": null,
+    "CrUID": null,
+    "NegativeAmount": null,
+    "LogMessage": "{\"message\": \"Journal line transaction successfully generated in Epicor\",\"LineUID\": \"1138\",\"GLAccount\": \"074-2005-0000-000-00000000-0000-00000000-0-000-99999995\",\"Amount\": \"6000000\"}",
+    "StatusCode": 1
+  },
+  {
+    "id": 1138,
+    "Company": "2005",
+    "BookID": "MAINP",
+    "JournalCode": "DJ",
+    "CurrencyCode": "TZS",
+    "TrxCtrlNum": "Actualctr1138",
+    "ApplyDate": "2017-06-30T00:00:00",
+    "Description": "Description1138",
+    "DebitAcc": null,
+    "PositiveAmount": null,
+    "DrUID": null,
+    "CreditAcc": null,
+    "CrUID": null,
+    "NegativeAmount": null,
+    "LogMessage": "{\"message\": \"Journal line transaction successfully generated in Epicor\",\"LineUID\": \"1138\",\"GLAccount\": \"074-2005-512A-201-00000000-0000-C3801S04-1-10A-13300012\",\"Amount\": \"6000000\"}",
+    "StatusCode": 1
+  }
+];
+
 /**
  *
  * declare the route
  */
-app.post('/push', function(req, res) {
-  var myName = req.body.name;
-  exchange.publish('my_queue1', {data: myName});
-  console.log("publish on RabbitMQ done..", req.body.name);
+app.get('/push', function(req, res) {
+
+  var opts = {
+    headers: bindingOptions
+  };
+
+  data.forEach(function(entry) {
+    exchange.publish('', entry, opts, function() {
+    });
+    console.log("publish on RabbitMQ done..", entry);
+  });
   res.redirect('/');
 });
 
